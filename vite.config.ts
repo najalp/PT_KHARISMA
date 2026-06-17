c2c0d57983c60a7345c1c99909328a7b46af75f2
najalp <najalp@users.noreply.github.com>
2026-06-18 02:31:07 +0800

    Fix base directory in vite config to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/PT_KHARISMA/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
